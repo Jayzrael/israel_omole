b17e90830af299f372462fe68226e6ad9518197d
Israel Omole <omolewayeolagd1997@gmail.com>
2023-11-08 14:13:26 +0100

    revamped portfolio

---
+++ src/content_option.js
@@ -0,0 +1,153 @@
+const logotext = "Israel";
+const meta = {
+  title: "Israel Omole",
+  description: "I’m Israel Omole a frontend developer,",
+};
+
+const introdata = {
+  title: "I’m Israel Omole",
+  animated: {
+    first: "I am a frontend developer",
+    second: "I develop responsive web apps",
+    third: "I build digital experiences",
+  },
+  description:
+    "I'm a frontend developer specialized in building (and occasionally designing) exceptional digital experiences. I am focused on building responsive web pages and applications for any niche.",
+  your_img_url: "/prfim.jpg",
+};
+
+const dataabout = {
+  title: "A bit about myself",
+  aboutme:
+    "Front End Developer with experience in the Fin-tech domains. Extensive experiences in HTML5, CSS3, SASS(SCSS),JavaScript and React Js. Proven track record in Analysis, Design, Development, Testing, and Implementation of Applications, Application Maintenance, and Project Deliverables. Detail-oriented with ability to own a project, follow complex instructions, diagnose and resolve issues. Possess problem-solving skills with a career objective to create stunning interactive websites that are visually appealing as well as user-friendly.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "JavaScript",
+    value: 80,
+  },
+  {
+    name: "ReactJs",
+    value: 70,
+  },
+  {
+    name: "Git",
+    value: 85,
+  },
+  {
+    name: "Tailwind css",
+    value: 85,
+  },
+  {
+    name: "Rest API",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "As a UI/UX designer, I can provide a variety of services to help businesses create intuitive and engaging digital experiences for their users. Some of the specific services I can offer include:",
+    list1:
+      "(1) User research and analysis - I can conduct user research to understand the needs, preferences, and behaviors of your target audience. This research can inform the design of your product and ensure that it meets the needs of your users.",
+    list2:
+      "(2) User interface design - I can create visually appealing and intuitive user interfaces for web and mobile applications using design software like Figma. This includes designing the layout, typography, color schemes, and visual elements of the interface.",
+    list3:
+      "(3) Prototyping and testing - I can create interactive prototypes of your product to test with users and gather feedback. This helps identify usability issues and ensures that the final product meets the needs of your users.",
+    list4:
+      "(4) Design system development - I can create a design system that ensures consistency across all aspects of your product, from typography and color to icons and animations. This system can be used to guide future design decisions and ensure a cohesive user experience.",
+    list5:
+      "(5) Collaborating with developers - I can work closely with developers to ensure that the final product meets both the design and technical requirements. This includes providing design specifications, answering questions, and addressing any design or technical issues that arise during the development process.",
+    P: "Overall, my goal as a UI/UX designer is to create digital experiences that are not only visually appealing but also intuitive and user-friendly, while meeting the business goals of the client.",
+  },
+  // {
+  //   title: "Mobile Apps",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  // },
+  // {
+  //   title: "Wordpress Design",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  // },
+];
+
+const dataportfolio = [
+  {
+    img: "/gymland.png",
+    desctiption: "Landing page built with React, TypeScript and vite",
+    link: "https://gym-land.vercel.app/",
+  },
+  {
+    img: "/riby-app.png",
+    desctiption: "Full fintech website built with React",
+    link: "https://riby.ng/",
+  },
+  // {
+  //   img: "/dashb.png",
+  //   desctiption: "Desktop-only Dashboard",
+  //   link: "https://riby-ticketing-app.vercel.app",
+  // },
+  {
+    img: "/nextapp.png",
+    desctiption: "Full Stack AI NextJs App, built with Nextjs 13 and MongoDb",
+    link: "https://promptai-swart.vercel.app/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "omolewayeolagd1997@gmail.com",
+  YOUR_FONE: "+2349030038597",
+  description:
+    "If you're looking to create an intuitive and engaging digital experience with a web app for your users, please don't hesitate to contact me! I'm excited to collaborate with you to bring your vision to life.",
+
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
